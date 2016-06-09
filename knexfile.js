43fe628aac652d39e805f0498fa155f644691b83
Chad Wilcomb <chadwilcomb@gmail.com>
2016-06-09 10:39:33 -0400

    Add Heroku configuration

--- knexfile.js
+++ knexfile.js
@@ -7,8 +7,18 @@ module.exports = {
       port     : '5432',
       user     : 'cwilcomb',
       // password : 'foosecret',
-      // database : 'nfldb',
       database : 'auctiondrafter_dev',
+      // charset  : 'UTF8_GENERAL_CI'
+    }
+  },
+  heroku: {
+    client: 'postgresql',
+    connection: {
+      host     : 'ec2-54-227-240-164.compute-1.amazonaws.com',
+      port     : '5432',
+      user     : 'lgcujwsugdubeu',
+      password : '5jHz9aOfO3QfKcU35P2MsecJlg',
+      database : 'd4ev8ls99895h8',
       // charset  : 'UTF8_GENERAL_CI'
     }
   }
--- src/app.js
+++ src/app.js
@@ -2,16 +2,9 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    // db  = require('./db'),
     router = require('./router');
-    // config = require('../knexfile.js'),
-    // env = 'development',
-    // knex = require('knex')(config[env]),
-    // bookshelf = require('bookshelf')(knex);
 
-// app.set('bookshelf', bookshelf);
-
-var port = 4000;
+var port = process.env.PORT || 4000;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
