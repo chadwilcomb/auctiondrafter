7fbf63c21ce30a76ba8bb8266bc4e5e0d71d18ef
Chad Wilcomb <chadwilcomb@gmail.com>
2016-06-09 12:19:39 -0400

    Updated Heroku Postgres info

--- knexfile.js
+++ knexfile.js
@@ -6,20 +6,9 @@ module.exports = {
       host     : 'localhost',
       port     : '5432',
       user     : 'cwilcomb',
-      // password : 'foosecret',
       database : 'auctiondrafter_dev',
       // charset  : 'UTF8_GENERAL_CI'
     }
   },
-  heroku: {
-    client: 'postgresql',
-    connection: {
-      host     : 'ec2-54-227-240-164.compute-1.amazonaws.com',
-      port     : '5432',
-      user     : 'lgcujwsugdubeu',
-      password : '5jHz9aOfO3QfKcU35P2MsecJlg',
-      database : 'd4ev8ls99895h8',
-      // charset  : 'UTF8_GENERAL_CI'
-    }
-  }
+  heroku: process.env.DATABASE_URL
 }
