6b1540083ab4806eea9210d1dbb0447a367dba0b
Chad Wilcomb <chadwilcomb@gmail.com>
2016-06-14 21:41:56 -0400

    Refactor app to add Redux TodoMVC example as web client

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict'
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    router = require('./router'),
+    webpack = require('webpack'),
+    webpackDevMiddleware = require('webpack-dev-middleware'),
+    webpackHotMiddleware = require('webpack-hot-middleware'),
+    config = require('./webpack.config')
+
+var port = process.env.PORT || 4000;
+
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
+
+router.initialize(app);
